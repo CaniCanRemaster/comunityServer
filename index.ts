a4899bdd9a4e4c519a04d14304faf33b4ff83dc6
CaniCanRemaster <canicanremasterizado@gmail.com>
2023-02-10 10:07:16 -0600

    stable version

--- index.ts
+++ index.ts
@@ -9,28 +9,32 @@ let configurationJson = JSON.parse(fs.readFileSync(__dirname + "/configuration.j
 
 async function start() {
 
-    let minecraftServer = childProcess.exec(`${__dirname}/minecraftBedrockServer/bedrock_server.exe`, { encoding: "utf-8" });
-    minecraftServer.on("message", (message) => {
-        console.log("message", message);
-    });
+    try {
+        let minecraftServer = childProcess.exec(`${__dirname}/minecraftBedrockServer/bedrock_server.exe`, { encoding: "utf-8" });
+        minecraftServer.on("message", (message) => {
+            console.log("message", message);
+        });
 
-    minecraftServer.on("spawn", ()=>{
-        console.log("spawn");
-    });
+        minecraftServer.on("spawn", () => {
+            console.log("spawn");
+        });
 
-    await minecraftBot.start(configurationJson, receiveMessageFromMinecraft);
-    await discordBot.start(configurationJson, receiveMessageFromDiscord);
+        await minecraftBot.start(configurationJson, receiveMessageFromMinecraft);
+        await discordBot.start(configurationJson, receiveMessageFromDiscord);
 
-    function receiveMessageFromDiscord(message: any) {
-        //console.log("-->Message from discord", message);
-        //minecraft.writeMinecraftMessage(configurationJson, message);
-    }
-
-    function receiveMessageFromMinecraft(message: any) {
-        //console.log("-->Message from minecraft", message);
-        if (message.source_name) {
-            discordBot.writeDiscordMessage(configurationJson, `<${message.source_name}>: ${message.message}`);
+        function receiveMessageFromDiscord(message: any) {
+            //console.log("-->Message from discord", message);
+            //minecraft.writeMinecraftMessage(configurationJson, message);
         }
+
+        function receiveMessageFromMinecraft(message: any) {
+            //console.log("-->Message from minecraft", message);
+            if (message.source_name) {
+                discordBot.writeDiscordMessage(configurationJson, `<${message.source_name}>: ${message.message}`);
+            }
+        }
+    } catch {
+
     }
 
 }
