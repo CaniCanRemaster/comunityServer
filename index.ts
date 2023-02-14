37a81736a19017571854f5c8b849b7c62205d1ae
CaniCanRemaster <canicanremasterizado@gmail.com>
2023-02-13 20:54:20 -0600

    Backup

--- index.ts
+++ index.ts
@@ -3,35 +3,38 @@ console.log("-->Starting software");
 import * as minecraftServer from "./minecraft.server";
 import * as minecraftBot from "./minecraft.bot";
 import * as discordBot from "./dicord.bot";
+import * as dotenv from "dotenv";
 import * as fs from "fs";
 
+dotenv.config();
 let configurationJson = JSON.parse(fs.readFileSync(__dirname + "/configuration.json", { encoding: "utf-8" }));
+configurationJson["discord"]["bot"]["token"] = process.env["token"];
 
 async function start() {
 
-    let minecraftServerInstance = await minecraftServer.start();
-    console.log("-->Minecraft server: ", minecraftServerInstance);
+    //let minecraftServerInstance = await minecraftServer.start();
+    //console.log("-->Minecraft server: ", minecraftServerInstance);
 
-    //setTimeout(async function(){
-        //await minecraftBot.start(configurationJson, receiveMessageFromMinecraft);
-        //await discordBot.start(configurationJson, receiveMessageFromDiscord);
-    //}, 20000);
+    setTimeout(async function () {
+        await minecraftBot.start(configurationJson, receiveMessageFromMinecraft);
+        await discordBot.start(configurationJson, receiveMessageFromDiscord);
+    }, 20000);
 
-    // function receiveMessageFromDiscord(message: any) {
-    //     console.log("-->Message from discord", message);
-    //     minecraft.writeMinecraftMessage(configurationJson, message);
-    // }
+    function receiveMessageFromDiscord(message: any) {
+        console.log("-->Message from discord", message);
+        minecraftBot.writeMinecraftMessage(configurationJson, message);
+    }
 
-    // function receiveMessageFromMinecraft(message: any) {
-    //     //console.log("-->Message from minecraft", message);
-    //     if (message.source_name) {
-    //         discordBot.writeDiscordMessage(configurationJson, `<${message.source_name}>: ${message.message}`);
-    //     }
-    //}
+    function receiveMessageFromMinecraft(message: any) {
+        //console.log("-->Message from minecraft", message);
+        if (message.source_name) {
+            discordBot.writeDiscordMessage(configurationJson, `<${message.source_name}>: ${message.message}`);
+        }
+    }
 
 }
 
-setInterval(()=>console.log("-->Executing..."), 360000);
+setInterval(() => console.log("-->Executing..."), 360000);
 
 start();
 
